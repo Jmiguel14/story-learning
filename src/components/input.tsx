71d7c2299e9ff2bc3a646f9216addedc178176e0
Miguel Jurado <migueljurado74@gmail.com>
2025-09-20 18:20:28 -0500

    Enhance Input component styles by adding a border for non-dark mode, improving visual consistency

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,8 +7,9 @@ export default function Input({
   return (
     <InputUi
       className={clsx(
-        "mt-3 block rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white",
-        "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25"
+        "mt-3 block rounded-lg bg-white/5 px-3 py-1.5 text-sm/6 text-white",
+        "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25",
+        "not-dark:border-gray-500 not-dark:border-1"
       )}
       {...props}
     />
