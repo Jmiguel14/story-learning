e2c0f80d9571f053da70f6bfc77aac2e6badc2f6
Miguel Jurado <migueljurado74@gmail.com>
2025-10-27 23:06:31 -0500

    Revamp TeamPage layout and content; introduce team member profiles with images and descriptions for enhanced engagement and clarity.

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,16 +1,28 @@
 export default function TeamPage() {
   return (
-    <div className="p-4 flex flex-col gap-4 justify-center">
-      <p>
-        Nuestro equipo desarrollo la planificación de la aplicación y funcionalidades para mejorar el aprendizaje de el lenguaje Español con divertidas leyendas y actividades interactivas.
+    <div className="p-4 flex flex-col gap-4 justify-center items-center">
+      <h1 className="text-2xl font-bold">Conoce nuestro equipo</h1>
+      <p className="text-gray-600 dark:text-gray-400">
+        Nuestro equipo desarrolló una página web educativa que busca facilitar
+        el aprendizaje de Lengua y Literatura mediante actividades didácticas,
+        lúdicas y personalizadas. A través de leyendas, juegos interactivos y ejercicios dinámicos, se promueve el desarrollo de la lectura, la escritura y la comprensión de una forma entretenida y accesible.
       </p>
-      <ul>
-        <li>
-          <h2>Joel Vaca</h2>
-          <h2>Johanna Chica</h2>
-          <h2>Yuneidy Salcedo</h2>
-        </li>
-      </ul>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-6 flex flex-col">
+          <img src="/images/team/Joel.jpeg" alt="Joel Vaca" className="w-52 h-52 object-cover rounded-full mb-4 self-center" />
+          <h2 className="text-lg font-semibold mb-2 text-gray-900 dark:text-white">Joel Vaca</h2>
+          <p className="text-gray-600 dark:text-gray-400">
+            Estudiante... creativo y proactivo.
+          </p>
+        </div>
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-6 flex flex-col">
+          <img src="/images/team/Sofia.jpeg" alt="Sofia Chica" className="w-52 h-52 object-cover rounded-full mb-4 self-center" />
+          <h2 className="text-lg font-semibold mb-2 text-gray-900 dark:text-white">Sofia Chica</h2>
+          <p className="text-gray-600 dark:text-gray-400">
+            Estudiante... creativo y proactivo.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
